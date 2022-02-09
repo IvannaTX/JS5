7736cd95e58cfd31f5f9ac4a434bf9d2e8865c25
Ivanna Contreras <ivanna.aws@gmail.com>
2022-02-09 08:53:11 -0600

    Udemy Course

--- starter/script.js
+++ starter/script.js
@@ -14,18 +14,24 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 //Geolocation with two callbacks: sucess and error
 if (navigator.geolocation) //Tests our function
-navigator.geolocation.getCurrentPosition(function(position){
-    // console.log(position); we are only wanting long&lay
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+         // console.log(position); we are only wanting long&lay
+            const {latitude} = position.coords;
+            const {longitude} = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView([coords], 13); //13 means zoom
+    // L stand for namespace like intl/has a couple map methods
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([coords])
+    .addTo(map) //available as a global variable
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 }, function(){
